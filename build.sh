16e46fe81210d9e4f5a981a72818ea28e6079100
Ankit <saharantech5566@gmail.com>
2025-07-28 13:26:24 +0530

    second

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-
 #!/usr/bin/env bash
+
+set -x  # print all commands
+set -e  # exit on first error
+
 pip install --upgrade pip setuptools wheel
-pip install --prefer-binary -r requirements.txt
+pip install --verbose --no-cache-dir -r requirements.txt
